74f94ea798941abb858117402c677c511ce56d2e
James <jamohgamer@gmail.com>
2024-04-24 17:53:47 +0300

    complete 1/2

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -6,6 +6,10 @@ import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
-
+  function Header(props) {
+    return <h1>hello from the Header!</h1>;
+  }
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
+
+export default Header;
